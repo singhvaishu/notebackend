b1456acdf173c1d0cfa9e79f7fd43e291d3ca65c
Vaishali Singh <94376468+singhvaishu@users.noreply.github.com>
2024-11-24 23:16:20 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { register, login } = require('../controllers/authController');
 const router = express.Router();
 
-router.post('/signup', register);
+router.post('/', register);
 router.post('/login', login);
 
 module.exports = router;
